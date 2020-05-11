c115c3ad6fdf828924120b0987bd8e2d854720cc
ekv <srbincar@gmail.com>
2020-05-11 19:47:50 +0200

    TFT user list and message update

--- data/ritoUserList.js
+++ data/ritoUserList.js
@@ -1,71 +1,98 @@
 const userList = [{
 		summonerName: 'ekv',
 		discordName: '<@!143490597804703745>',
-		lastCheck: 1581380434138
+		lastCheck: 1581380434138,
+		tftLastCheck: 1581380434138
 	},{
 		summonerName: 'ProblematicSushi',
 		discordName: '<@!256513486559903744>',
-		lastCheck: 1581359155864
+		lastCheck: 1581359155864,
+		tftLastCheck: 1581380434138
 	},{
 		summonerName: 'Duc',
 		discordName: '<@!475391468157141002>',
-		lastCheck: 1581359155864
+		lastCheck: 1581359155864,
+		tftLastCheck: 1581380434138
 	},{
 		summonerName: 'Kiseli vaskrse',
 		discordName: '<@!263667003452162049>',
-		lastCheck: 1581379333527
+		lastCheck: 1581379333527,
+		tftLastCheck: 1581380434138
 	},{
 		summonerName: 'EmptySpaces',
 		discordName: '<@!289570348389761024>',
-		lastCheck: 0
+		lastCheck: 1581380434138,
+		tftLastCheck: 1581380434138
 	},{
 		summonerName: '12InchesOfPain',
 		discordName: '<@!307115937947975681>',
-		lastCheck: 1581359155864
+		lastCheck: 1581359155864,
+		tftLastCheck: 1581380434138
 	},{
 		summonerName: 'Trypticon10',
 		discordName: '<@!263411859414188042>',
-		lastCheck: 1581296998547
+		lastCheck: 1581296998547,
+		tftLastCheck: 1581380434138
 	},{
 		summonerName: 'StexoCaster',
 		discordName: '<@!177540752740581376>',
-		lastCheck: 1581296998547
+		lastCheck: 1581296998547,
+		tftLastCheck: 1581380434138
 	},{
 		summonerName: 'Maslacak',
 		discordName: '<@!267807090712510467>',
-		lastCheck: 1581276218277
+		lastCheck: 1581276218277,
+		tftLastCheck: 1581380434138
 	},{
 		summonerName: 'DoomPajser',
 		discordName: '<@!271108421875400705>',
-		lastCheck: 1581296998547
+		lastCheck: 1581296998547,
+		tftLastCheck: 1581380434138
 	},{
 		summonerName: 'SyP3',
 		discordName: '<@!394970418299142144>',
-		lastCheck: 1581380434138
+		lastCheck: 1581380434138,
+		tftLastCheck: 1581380434138
 	},{
 		summonerName: 'Cao Desno Jaje',
 		discordName: '<@!567457463046111232>',
-		lastCheck: 1581380434138
+		lastCheck: 1581380434138,
+		tftLastCheck: 1581380434138
 	},{
 		summonerName: 'Cheeky Sneaky',
 		discordName: '<@!139185075240697856>',
-		lastCheck: 1581377701887
+		lastCheck: 1581377701887,
+		tftLastCheck: 1581380434138
 	},{
 		summonerName: 'Cao levo jaje',
 		discordName: '<@!288746781456728065>',
-		lastCheck: 1581380434138
+		lastCheck: 1581380434138,
+		tftLastCheck: 1581380434138
 	},{
 		summonerName: 'Ravmon',
 		discordName: '<@!596826809291505687>',
-		lastCheck: 1581380434138
+		lastCheck: 1581380434138,
+		tftLastCheck: 1581380434138
 	},{
 		summonerName: 'arQoZ',
 		discordName: '<@!325940582058819584>',
-		lastCheck: 0
+		lastCheck: 1581380434138,
+		tftLastCheck: 1581380434138
 	},{
 		summonerName: 'YT LinvuS',
 		discordName: '<@!296571256466309120>',
-		lastCheck: 0,
+		lastCheck: 1581380434138,
+		tftLastCheck: 1581380434138
+	},{
+		summonerName: 'Shusui',
+		discordName: '<@!391629680794533890>',
+		lastCheck: 1581380434138,
+		tftLastCheck: 1581380434138
+	},{
+		summonerName: 'Kobra Brcko',
+		discordName: '<@!654484804057956353>',
+		lastCheck: 1581380434138,
+		tftLastCheck: 1581380434138
 	}
 ];
 
